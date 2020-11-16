56f7e775cde6a5a83e6eacc88313d6b39e9868e3
Aleksandr Melkov <amelkov@compuproject.com>
2020-11-16 20:31:00 +0300

    first part of task

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // server.js
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 4000;
 
-app.use(express.static('./dist'));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.listen(PORT, function () {
     console.log(`Example app listening on port ${PORT}!`);
